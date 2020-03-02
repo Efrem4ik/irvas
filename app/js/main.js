c1a50433e5f7ef84dc9a405801d29ac2bbe15001
Efrem4ik <efrem4ik12@mail.ru>
2020-03-02 20:16:12 +0300

    end project

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
   // $('.menu__btn').on('click', function(){
   //   $('.menu__list').slideToggle();
@@ -11,30 +11,38 @@ $(function(){
   //   slidesToScroll: 4,
   // }); Обычный одиночный слайдер!
 
-	//  $('.slider-for').slick({
-	//   slidesToShow: 1,
-	//   slidesToScroll: 1,
-	//   arrows: false,
-	//   fade: true,
-	//   asNavFor: '.slider-nav'
-	// });
-	// $('.slider-nav').slick({
-	//   slidesToShow: 3,
-	//   slidesToScroll: 1,
-	//   asNavFor: '.slider-for',
-	//   dots: true,
-	//   centerMode: true,
-	//   focusOnSelect: true
-	// }); Двойной слайдер!
+  //  $('.slider-for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav'
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-for',
+  //   dots: true,
+  //   centerMode: true,
+  //   focusOnSelect: true
+  // }); Двойной слайдер!
 
 
-  // $('.wrapper .tab').on('click', function(event) {
-  // var id = $(this).attr('data-id');
-  //   $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
-  //   $('.wrapper .tabs').find('.tab').removeClass('active');
-  //   $(this).addClass('active');
-  //   $('#'+id).addClass('active-tab').fadeIn();
-  //   return false;
-  // }); Для табов!
+  $('.services__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.services__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.services__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+  $('.order__tabs .tab-o').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.order__tabs').find('.tab-item-o').removeClass('active-tab').hide();
+    $('.order__tabs .tabs').find('.tab-o').removeClass('active-o');
+    $(this).addClass('active-o');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
 
 });
